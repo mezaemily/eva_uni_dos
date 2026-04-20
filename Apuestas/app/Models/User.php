c0db6dddd7a20fbe7232641d79eef2f05ee32e88
Emily meza <mezaemily54@gmail.com>
2026-04-20 12:07:38 -0600

    jale
    jale

--- Apuestas/app/Models/User.php
+++ Apuestas/app/Models/User.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class User extends Authenticatable implements JWTSubject 
 {
     use Notifiable;
 
@@ -15,4 +17,32 @@ class User extends Authenticatable
     public function bets()         { return $this->hasMany(Bet::class); }
     public function transactions() { return $this->hasMany(Transaction::class); }
     public function isAdmin()      { return $this->role === 'admin'; }
+
+     /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+     /**
+     * Get the identifier that will be stored in the JWT token.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return an array with custom claims to be added to the JWT token.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
---
+++ Apuestas/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+ 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UsersController;
+
+use App\Http\Controllers\Api\AuthController;
+
+
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('jwt')->group(function () {
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::put('/user', [AuthController::class, 'updateUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
